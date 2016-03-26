5c54cd3f97820511b6af351f1c20b3a818f6a9c1
James Ko <jamesqko@gmail.com>
2016-03-26 13:18:58 -0400

    Sync changes from netbuild

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enableDelayedExpansion
 
 goto main
 
@@ -9,24 +9,46 @@ goto main
 
 pushd %1
 for /f "delims=" %%p in ('dir /a-d /b /s project.json') do (
-    dnu restore "%%p"
-    dnu build "%%p"
-    dnu pack "%%p"
+    :: Exclude bin/ and obj/ directories
+    echo %%p | findstr /i /c:bin /c:obj > NUL
+    if !ERRORLEVEL! NEQ 0 (
+        dnu restore "%%p"
+        dnu build "%%p"
+        dnu pack "%%p"
+    )
 )
 popd
 goto :EOF
 
-:main
+:checkInstalled
 
-:: Check for dnu
-where dnu > NUL 2>&1
+where %1 > NUL 2>&1
 if %ERRORLEVEL% NEQ 0 (
-    echo dnu wasn't found in your PATH! 1>&2
+    echo %1 wasn't found in your PATH! 1>&2
     echo See http://docs.asp.net/en/latest/getting-started/installing-on-windows.html for instructions on installing the DNX toolchain on your PC. 1>&2
     exit /b %ERRORLEVEL%
 )
+goto :EOF
+
+:runTests
+pushd %1
+for /f "delims=" %%p in ('dir /a-d /b /s project.json') do (
+    :: Exclude bin/ and obj/ directories
+    echo %%p | findstr /i /c:bin /c:obj > NUL
+    if !ERRORLEVEL! NEQ 0 dnx -p "%%p" test
+)
+popd
+goto :EOF
+
+:: Entry point
+:main
+
+:: Check for dnu and dnx
+call :checkInstalled dnu
+call :checkInstalled dnx
 
 :: Do the actual work
 cd %~dp0
 call :buildSubdir src
 call :buildSubdir test
+call :runTests test
